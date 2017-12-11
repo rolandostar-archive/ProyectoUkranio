89b5a06ca62f9bf2e32eed24663b278d883144be
pachecokun <mikorreomola@gmail.com>
2017-12-11 17:10:29 -0600

    queries a servidores

--- web_server/server.c
+++ web_server/server.c
@@ -2,6 +2,7 @@
 #include "mongoose.h"
 #include "../SocketDatagrama.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -16,21 +17,22 @@ static const char *s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
 
 char servers[16][255];
+int n_servers = 0;
 
 struct mg_str cb(struct mg_connection *c, struct mg_str file_name) {
 	char* f_name = (char*)malloc(file_name.len+5);
 
-	sprintf(f_name,"text/%s",file_name.p);
-	
+	sprintf(f_name,"www/text/%s",file_name.p);
+
 	file_name.len += 5;
 	file_name.p = f_name;
-	
+
 	return file_name;
 }
+SocketDatagrama s_send(0);
+SocketDatagrama s_recv(9444);
 
 void discover(){
-	SocketDatagrama s_send(0);
-	SocketDatagrama s_recv(9444);
 
 	s_send.activaBroadcast(true);
 
@@ -44,10 +46,16 @@ void discover(){
 	PaqueteDatagrama p1((char*)&data_ping,sizeof(data_ping),broadcast,9444);
 	cout << "Enviando ping inicial" << endl;
 	s_send.envia(p1);
-	while(1){	
+	s_recv.setTimeout(5,0);
+	n_servers = 0;
+	while(1){
 		cout << "Esperando respuestas..." << endl;
 		PaqueteDatagrama p3(sizeof(struct operacion));
-		s_recv.recibe(p3);
+		int r;
+		if((r=s_recv.recibe(p3))<=0){
+			break;
+		}
+		printf("codigo %d\n",r);
 		if(!strcmp(p3.obtieneDireccion(),"127.0.0.1")) {
 			cout << "Saltandome" << endl;
 			continue;
@@ -56,6 +64,58 @@ void discover(){
 		memcpy(&op_recv, p3.obtieneDatos(), sizeof(struct operacion));
 
 		printf("%d %d %s\n",op_recv.op,op_recv.v1,p3.obtieneDireccion());
+		if(op_recv.v1==1){
+			memcpy(servers[n_servers++],p3.obtieneDireccion(),16);
+		}
+	}
+	printf("%d servidores encontrados\n",n_servers);
+}
+
+void send_queries(char* query){
+	int i;
+	struct operacion op = {
+		1,
+		0,
+		n_servers,
+		""
+	};
+	memcpy(op.arg,query,strlen(query)+1);
+	for(i=0;i<n_servers;i++){
+		op.v1 = i+1;
+		PaqueteDatagrama p1((char*)&op,sizeof(op),servers[i],9444);
+		cout << "Enviando query a " << servers[i] << endl;
+		s_send.envia(p1);
+	}
+}
+
+void send_files(char*file){
+	
+}
+
+void get_search(){
+	int done = 0;
+	while(1){
+		cout << "Esperando respuestas..." << endl;
+		PaqueteDatagrama p3(sizeof(struct operacion));
+		if(s_recv.recibe(p3)<=0){
+			return;
+		}
+		if(!strcmp(p3.obtieneDireccion(),"127.0.0.1")) {
+			cout << "Saltandome" << endl;
+			continue;
+		}
+		struct operacion op_recv;
+		memcpy(&op_recv, p3.obtieneDatos(), sizeof(struct operacion));
+
+		printf("%d %d %s\n",op_recv.op,op_recv.v1,p3.obtieneDireccion());
+		if(op_recv.v1==3){
+			done++;
+		}else if(op_recv.v1==2){
+			//printf();
+		}
+		if(done==n_servers){
+			break;
+		}
 	}
 }
 
@@ -68,6 +128,8 @@ static void handle_search(struct mg_connection *nc, struct http_message *hm) {
 		mg_printf(nc, "%s", query);
 		printf("query: %s\n",query);
 		discover();
+		send_queries(query);
+		get_search();
 }
 
 static void ev_handler(struct mg_connection *nc, int ev, void *p) {
@@ -84,10 +146,10 @@ static void ev_handler(struct mg_connection *nc, int ev, void *p) {
     		mg_http_send_redirect(nc, 302, mg_mk_str("/"), mg_mk_str(NULL));
     	}
     	mg_file_upload_handler(nc, ev, p, cb);
-    	
+
 	}
-	
-	
+
+
 }
 int main(void) {
 	struct mg_mgr mgr;
